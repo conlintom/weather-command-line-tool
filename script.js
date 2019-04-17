22453987a7e6b6e212739902a6e51db0bb68c573
Conlin, Tom <tom.conlin@hpe.com>
2019-04-17 09:13:04 -0400

    added comments to the code

--- script.js
+++ script.js
@@ -2,12 +2,17 @@
 // use axios
 const axios = require("axios");
 
+// latitude and logitude variables
+var lat = "39.7456";
+var lon = "-97.0892";
 
 // use axios to make the request
-var result = axios.get('https://api.weather.gov/points/39.7456,-97.0892/forecast')
+var result = axios.get("https://api.weather.gov/points/" + lat + "," + lon + "/forecast")
   .then(function (result) {
+    // get the weather information for today
     console.log(result.data.properties.periods[0]);
   })
+  // catch request errors
   .catch(function (error) {
     console.log(error);
   });
