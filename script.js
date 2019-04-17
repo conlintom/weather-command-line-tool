4bb832eeeb1ea07be924a06567c097b69d7572ba
Conlin, Tom <tom.conlin@hpe.com>
2019-04-17 09:07:41 -0400

    updated to use axios to make the request to the weather api instead of using XMLHTTPRequests directly

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
 
+// use axios
+const axios = require("axios");
 
-// Create the resquest object
-var request = new XMLHttpRequest(); 
 
-// Create the lat lon variables
-// Take these from the user (command line) in the future
-var lat = 42.349200
-var lon = -71.04848
-
-// open the request to the weather services API using the /points/lat,lon/forecast function
-request.open("GET", "https://api.weather.gov/points/" + lat + "," + lon + "/forecast", true);
-
-// When the page loads
-request.onload = function() {
-    var data = JSON.parse(this.response);
-    var periods = data.properties.periods;
-    //console.log(Array.isArray(periods));
-    //console.log(periods.length)
-    console.log(periods);
-    // Check the request status
-    if (request.status >= 200 && request.status < 400){
-        for (var i = 0; i < periods.length; i++ ){
-            var per = periods[i]
-            console.log(per.detailedForecast);
-        }
-    } else {
-        console.log("It's not working!");
-    }
-}
-
-request.send();
+// use axios to make the request
+var result = axios.get('https://api.weather.gov/points/39.7456,-97.0892/forecast')
+  .then(function (result) {
+    console.log(result.data.properties.periods[0]);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
