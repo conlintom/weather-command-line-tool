74ca4534d82cb7159f4819f85fd114fc75163d48
Conlin, Tom <tom.conlin@hpe.com>
2019-04-18 17:09:34 -0400

    attept to add ink and react - need to explore options for using both modules in the code with node.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-
 // use axios
 const axios = require("axios");
+const ink = require("ink");
+const react = require("react");
+const reactDOM = require("react-dom/server");
+
+// console.log(react);
+// console.log(ink);
+console.log(reactDOM);
+
+class MyComponent extends react.Component{
+    render() {
+        return <div>Hello World</div>;
+    }
+}
+ReactDOM.reenderToString(<MyComponent/>, node);
+
 
 // latitude and logitude variables
 let lat = "39.7456";
@@ -9,8 +23,8 @@ let lon = "-97.0892";
 // use axios to make the request to the weather api
 axios.get("https://api.weather.gov/points/" + lat + "," + lon + "/forecast")
   .then(function (res) {
-    // get the weather information for today
-    console.log(res.data.properties.periods[0]);
+    // get the weather information for the current time period
+    //console.log(res.data.properties.periods[0]);
   })
   // catch request errors
   .catch(function (err) {
