2ccac3f4d67528760c67e353cf24eda8956ccf7b
Conlin, Tom <tom.conlin@hpe.com>
2019-05-01 09:30:13 -0400

    accept commandline arguments and add example if noen are supplied

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 
-// require('import-jsx')('./WeatherBar');
-
-const lat = process.argv[0];
-const lon = process.argv[1];
-
-const weatherBar = require('import-jsx')('./WeatherBar');
-
-weather = new weatherBar(lat, lon);
+require('import-jsx')('./WeatherBar');
