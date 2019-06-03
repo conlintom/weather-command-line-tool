02eae3e299fae345f97cf689e1677c816af2f41e
Conlin, Tom <tom.conlin@hpe.com>
2019-06-03 09:34:37 -0400

    removed console.logs from request module

--- Adapter/weather.js
+++ Adapter/weather.js
@@ -5,11 +5,9 @@ module.exports.readByCoordinates = function(lat, lon) {
         axios.get(`https://api.weather.gov/points/${lat},${lon}/forecast`)
             .then( res => {
                 resolve(res.data.properties.periods);
-                console.log("success");
             })
             .catch(err => {
                 reject(new Error(err.response.data['detail']));
-                console.log("error!!!");
             });
     });
 };
